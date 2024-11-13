98e0c105a5e65c5b56aba5881b9ab8ad0071281f
Lucas-d-Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2024-11-13 18:17:49 -0300

    Footer completo

--- resources/views/components/_partials/footer.blade.php
+++ resources/views/components/_partials/footer.blade.php
@@ -1,3 +1,32 @@
-<footer class="bg-primary p-5">
-    <p class="text-white">Lorem ipsum dolor sit.</p>
+<footer class="bg-primary p-5 d-flex flex-column align-items-center gap-5" id="contatos">
+    <h3 class="display-6 text-white">Contatos</h3>
+    <div class="row w-100 text-white">
+        <!-- E-mail -->
+        <div class="col-12 col-md-3 text-center mb-4">
+            <i class="bi bi-envelope-fill fs-1 mb-2 contato-icone"></i>
+            <div>E-mail</div>
+            <span>francisco.lucas.d.santos.barbosa@gmail.com</span>
+        </div>
+        
+        <!-- Telefone -->
+        <div class="col-12 col-md-3 text-center mb-4">
+            <i class="bi bi-telephone-fill fs-1 mb-2 contato-icone"></i>
+            <div>Telefone</div>
+            <span>(85) 99941-1992</span>
+        </div>
+        
+        <!-- LinkedIn -->
+        <div class="col-12 col-md-3 text-center mb-4">
+            <i class="bi bi-linkedin fs-1 mb-2 contato-icone"></i>
+            <div>LinkedIn</div>
+            <a href="https://www.linkedin.com/in/francisco-barbosa-aa16b82b7/" target="_blank" class="text-white">linkedin.com/in/franciscobarbosa</a>
+        </div>
+
+        <!-- Github -->
+        <div class="col-12 col-md-3 text-center mb-4">
+            <i class="bi bi-github fs-1 mb-2 contato-icone"></i>
+            <div>Github</div>
+            <a href="https://github.com/Lucas-d-Barbosa" target="_blank" class="text-white">github.com/Lucas-d-Barbosa</a>
+        </div>
+    </div>
 </footer>
