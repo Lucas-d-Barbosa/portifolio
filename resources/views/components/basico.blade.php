c74e02684dd28943a82c1394b910e65a13cedbc4
Lucas-d-Barbosa <francisco.lucas.d.santos.barbosa@gmail.com>
2024-11-13 16:08:52 -0300

    Seção sobre mim, habilidade e apresentação inicial tornadas responsivas e alteração da coloração da primeira seção

--- resources/views/components/basico.blade.php
+++ resources/views/components/basico.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/basico.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
     <title>@yield('title')</title>
 </head>
 <body>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,28 +26,65 @@
 @section('title', 'Home')
 @section('conteudo')
 <main class="flex-grow-1">
-    <section class="p-4 row d-flex align-items-center section-container w-100 bg-body-secondary">
-        <div class="col-6 d-flex justify-content-center" style="text-align: center">
-            <div class="col-7">
-                <h2 class="display-6">
-                    Olá, meu nome é Lucas Barbosa! 
+    <section class="p-4 row d-flex align-items-center section-container w-100 bg-primary no-gutters">
+        <div class="col-12 col-sm-6 d-flex justify-content-center" style="text-align: center">
+            <div class="col-12">
+                <h2 class="display-5 display-sm-4 text-center text-white">
+                    Olá, meu nome é Lucas Barbosa!
                 </h2>
-                <p class="text-primary fs-4">Desenvolvedor Back-end</p>
-                <button class="btn btn-primary">Download CV</button>
-                <button class="btn btn-outline-primary">Entrar em contato</button>
+                <p class="text-light fs-5 fs-sm-4 fs-md-3 text-center">Desenvolvedor Back-end</p>                
+                <div class="col-12 d-flex justify-content-center gap-2 flex-column flex-md-row">
+                    <button class="btn btn-success">Download CV</button>
+                    <button class="btn btn-outline-light">Entrar em contato</button>
+                </div>
+                
             </div>
         </div>
-        <div class="col-6 d-flex justify-content-center">
-            <div class="image-container border-primary">
+        <div class="col-12 col-sm-6 d-flex justify-content-center">
+            <div class="image-container border-white bg-primary-subtle">
                 <img class="rounded responsive-image" src="{{ asset('images/img1.png') }}" alt="Logo">
             </div>
         </div>
     </section>
-    <section class="p-4 row d-flex flex-column align-items-center justify-content-center g-4 section-container w-100">
-        <h3 class="display-6" style="text-align: center">Um pouco sobre mim</h3>
-        <div class="col-8 d-flex justify-content-center p-2">
-            <p class="text-dark fs-4">Sou desenvolvedor Back-end e tenho bastante experiência em tecnologias como PHP, Laravel e tenho ótimos conhecimentos em Java. Sou extremamente dedicado ao desenvolvimento de aplicações web utilizando técnicas modernas. Apesar de ter preferência pela programação Back-end, também conheço tecnologias Front-end como HTML e Bootstrap 5 e Vue.js.</p>
+    
+    <section class="p-4 section-container w-100 d-flex flex-column align-items-center gap-4 justify-content-center bg-light">
+        <h3 class="display-6 text-center ">Um pouco sobre mim</h3>
+        <div class="col-12 col-md-8 d-flex justify-content-center p-2 mx-auto">
+            <p class="text-dark fs-4 fs-sm-5 fs-md-6 text-muted">
+                Sou desenvolvedor Back-end e tenho bastante experiência em tecnologias como PHP, Laravel e tenho ótimos conhecimentos em Java. Sou extremamente dedicado ao desenvolvimento de aplicações web utilizando técnicas modernas. Apesar de ter preferência pela programação Back-end, também conheço tecnologias Front-end como HTML e Bootstrap 5 e Vue.js.
+            </p> 
         </div>
     </section>
+
+    <section class="p-4 section-container w-100 d-flex flex-column align-items-center justify-content-center bg-white">
+        <h3 class="display-6 text-center">Habilidades</h3>
+        <ul class="list-unstyled row row-b w-100 justify-content-center">
+            <li class="col-12 col-md-4 mb-4 text-center habilidade-item">
+                <span class="badge bg-primary fs-4 w-100 text-center">PHP</span>
+            </li>
+            <li class="col-12 col-md-4 mb-4 text-center habilidade-item">
+                <span class="badge bg-success fs-4 w-100 text-center">Laravel</span>
+            </li>
+            <li class="col-12 col-md-4 mb-4 text-center habilidade-item">
+                <span class="badge bg-warning fs-4 w-100 text-center">Java</span>
+            </li>
+            <li class="col-12 col-md-4 mb-4 text-center habilidade-item">
+                <span class="badge bg-danger fs-4 w-100 text-center">HTML</span>
+            </li>
+            <li class="col-12 col-md-4 mb-4 text-center habilidade-item">
+                <span class="badge bg-info fs-4 w-100 text-center">CSS</span>
+            </li>
+            <li class="col-12 col-md-4 mb-4 text-center habilidade-item">
+                <span class="badge bg-dark fs-4 w-100 text-center">Bootstrap 5</span>
+            </li>
+        </ul>
+    </section>
+    
+    
+    
+    
+    
+    
+
 </main>
 @endsection
